fa3154a54a0f28294e18b2f57598d013ac7c7845
odwa <odwa.menziwa@jumpco.io>
2022-10-24 13:22:11 +0200

    with interface not completed using infer constrait not working as intended

---
+++ app/src/main/java/android/com/jamsand/io/appointmentreminder/AddAppointmentActivity.java
@@ -0,0 +1,27 @@
+package android.com.jamsand.io.appointmentreminder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class AddAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_appointment);
+    }
+
+    public void edittextDate(View view) {
+    }
+
+    public void btnCancel(View view) {
+    }
+
+    public void btnAddAppointment(View view) {
+    }
+
+    public void edittxtTime(View view) {
+    }
+}
